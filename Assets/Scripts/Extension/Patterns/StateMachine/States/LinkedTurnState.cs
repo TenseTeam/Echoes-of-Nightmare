f47575e7ada9d39dd478a94709d997a8d75af989
zVoyder <zvoyder@protonmail.com>
2023-05-10 23:31:54 +0200

    fixing state machine

--- Assets/Scripts/Extension/Patterns/StateMachine/States/LinkedTurnState.cs
+++ Assets/Scripts/Extension/Patterns/StateMachine/States/LinkedTurnState.cs
@@ -2,28 +2,17 @@
 {
     using Extension.Patterns.StateMachine.Interfaces;
 
-    public abstract class LinkedTurnState : IEventState
+    public abstract class LinkedTurnState : State
     {
-        public string StateName { get; private set; }
         public TurnStateMachine RelatedStateMachine { get; private set; }
 
         /// <summary>
         /// Initializes a new instance of the State class with the specified name.
         /// </summary>
         /// <param name="name">Name of the state.</param>
-        protected LinkedTurnState(string name, TurnStateMachine relatedStateMachine)
+        protected LinkedTurnState(string name, TurnStateMachine relatedStateMachine) : base(name)
         {
-            StateName = name;
             RelatedStateMachine = relatedStateMachine;
         }
-
-        /// <inheritdoc/>
-        public abstract void Enter();
-
-        /// <inheritdoc/>
-        public abstract void Exit();
-
-        /// <inheritdoc/>
-        public abstract void Process();
     }
 }
