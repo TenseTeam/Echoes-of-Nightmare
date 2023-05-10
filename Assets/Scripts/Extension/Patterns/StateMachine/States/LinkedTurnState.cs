1060fc60f0b06dd6ed15ab19a67c2c3fc0e05c16
zVoyder <zvoyder@protonmail.com>
2023-05-10 23:20:47 +0200

    added features for managing the SubStateMachines

---
+++ Assets/Scripts/Extension/Patterns/StateMachine/States/LinkedTurnState.cs
@@ -0,0 +1,29 @@
+﻿namespace Extension.Patterns.StateMachine
+{
+    using Extension.Patterns.StateMachine.Interfaces;
+
+    public abstract class LinkedTurnState : IEventState
+    {
+        public string StateName { get; private set; }
+        public TurnStateMachine RelatedStateMachine { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the State class with the specified name.
+        /// </summary>
+        /// <param name="name">Name of the state.</param>
+        protected LinkedTurnState(string name, TurnStateMachine relatedStateMachine)
+        {
+            StateName = name;
+            RelatedStateMachine = relatedStateMachine;
+        }
+
+        /// <inheritdoc/>
+        public abstract void Enter();
+
+        /// <inheritdoc/>
+        public abstract void Exit();
+
+        /// <inheritdoc/>
+        public abstract void Process();
+    }
+}
---
+++ Assets/Scripts/Extension/Patterns/StateMachine/SubTurnStateMachine.cs
@@ -0,0 +1,16 @@
+﻿namespace Extension.Patterns.StateMachine
+{
+    using System.Collections.Generic;
+    using UnityEngine;
+
+    public class SubTurnStateMachine : TurnStateMachine
+    {
+        public TurnStateMachine ParentStateMachine { get; private set; }
+
+        protected void InitStates(TurnStateMachine parentStateMachine)
+        {
+            ParentStateMachine = parentStateMachine;
+            InitStates();
+        }
+    }
+}
