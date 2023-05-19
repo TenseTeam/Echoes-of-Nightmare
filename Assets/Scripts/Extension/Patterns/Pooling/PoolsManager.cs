ea04793d2f4dfbb8cdc4f69bef2ff48ccc3a1f74
zVoyder <zvoyder@protonmail.com>
2023-05-20 01:12:38 +0200

    fixed poolsmanager

--- Assets/Scripts/Extension/Patterns/Pooling/PoolsManager.cs
+++ Assets/Scripts/Extension/Patterns/Pooling/PoolsManager.cs
@@ -1,14 +1,11 @@
 ﻿namespace Extension.Patterns.ObjectPool
 {
-    using System.Collections.Generic;
     using UnityEngine;
-    using Extension.Transform;
-    using Extension.Patterns.ObjectPool.Interfaces;
     using Extension.Generic.Serializable;
 
     public class PoolsManager : MonoBehaviour
     {
         [field: SerializeField]
-        public SerializableDictionary<int, string> Pools { get; private set; }
+        public SerializableDictionary<string, Pool> Pools { get; private set; }
     }
 }
