9e3587a3349ae3e279f810e62f1ebce50f63ef1b
Zigrash <lorenzorocco1995@gmail.com>
2023-05-26 03:42:32 +0200

    Added all Characters Animations and abilities

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,89 +1,51 @@
 namespace ProjectEON.PlayerSystem
 {
+    using System.Collections;
+    using System.Collections.Generic;
     using UnityEngine;
 
-    [RequireComponent(typeof(Animator))]
     public class PlayerMovement : MonoBehaviour
     {
-        [SerializeField]
-        private float _speed;
+        public float speed;
+        public float groundDist;
 
-        private Rigidbody _rigidBody;
-        private SpriteRenderer _sprite;
-        private Animator _animator;
-        private Vector3 _moveDirection;
-
-        [Header("Slope Handling")]
-        [SerializeField]
-        private float _maxSlopeAngle;
-
-        [SerializeField]
-        private float _playerHeight;
-
-        private RaycastHit _slopeHit;
-        private bool _isExitingSlope;
-
-        private void Start()
+        public LayerMask terrainLayer;
+        public Rigidbody rb;
+        public SpriteRenderer sr;
+        // Start is called before the first frame update
+        void Start()
         {
-            _rigidBody = GetComponent<Rigidbody>();
-            _animator = GetComponent<Animator>();
-            _sprite = GetComponentInChildren<SpriteRenderer>();
+            rb = gameObject.GetComponent<Rigidbody>();
         }
 
-        public void Move(float vertical, float horizontal)
+        // Update is called once per frame
+        void Update()
         {
-            if (Mathf.Abs(_rigidBody.velocity.x) > 0.001f || Mathf.Abs(_rigidBody.velocity.z) > 0.001f)
-                _animator.SetBool("isMoving", true);
-            else
-                _animator.SetBool("isMoving", false);
-
-            _moveDirection = new Vector3(horizontal * (_speed * 100) * Time.deltaTime, _rigidBody.velocity.y, vertical * (_speed * 100) * Time.deltaTime);
-            //m_RigidBody.velocity = (Vector3.forward * vertical + Vector3.right * horizontal) * (m_Speed * 100) * Time.deltaTime;
-
-            // on slope
-            if (OnSlope())
+            RaycastHit hit;
+            Vector3 castPos = transform.position;
+            castPos.y += 1;
+            if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
             {
-                if (vertical == 0 && horizontal == 0)
-                    _rigidBody.isKinematic = true;
-                else
-                    _rigidBody.isKinematic = false;
-
-                _rigidBody.AddForce(GetSlopeMoveDirection() * _speed * 20f, ForceMode.Force);
-
-                if (_rigidBody.velocity.y > 0)
-                    _rigidBody.AddForce(Vector3.down * 100f, ForceMode.Force);
+                if (hit.collider != null)
+                {
+                    Vector3 movePos = transform.position;
+                    movePos.y = hit.point.y + groundDist;
+                    transform.position = movePos;
+                }
             }
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            Vector3 moveDir = new Vector3(x, 1, y);
+            rb.velocity = moveDir * speed;
 
-            _rigidBody.velocity = _moveDirection;
-            // turn gravity off while on slope
-            //m_RigidBody.useGravity = !OnSlope();
-            _sprite.flipX = _rigidBody.velocity.x < 0;
-        }
-
-        private void SpeedControl()
-        {
-            // limiting speed on slope
-            if (OnSlope() && !_isExitingSlope)
+            if (x != 0 && x < 0)
             {
-                if (_rigidBody.velocity.magnitude > _speed)
-                    _rigidBody.velocity = _rigidBody.velocity.normalized * _speed;
+                sr.flipX = true;
             }
-        }
-
-        private bool OnSlope()
-        {
-            if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit, _playerHeight * 0.5f + 0.3f))
+            else if (x != 0 && x > 0)
             {
-                float angle = Vector3.Angle(Vector3.up, _slopeHit.normal);
-                return angle < _maxSlopeAngle && angle != 0;
+                sr.flipY = false;
             }
-
-            return false;
-        }
-
-        private Vector3 GetSlopeMoveDirection()
-        {
-            return Vector3.ProjectOnPlane(_moveDirection, _slopeHit.normal).normalized;
         }
     }
 }
